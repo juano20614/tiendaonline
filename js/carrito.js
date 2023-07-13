8fdce5e3e817fc7082a07f7ef1e44fd8da331c9c
Juan Marulanda <juanmarucasta@gmail.com>
2023-07-13 09:39:30 -0500

    Agregada la funcionalidad carrito

---
+++ js/carrito.js
@@ -0,0 +1,46 @@
+let  boton1 = document.getElementById("carrito1");
+let pElement= document.getElementById("contador")
+let  contador=0;
+
+boton1.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton2 = document.getElementById("carrito2");
+
+
+boton2.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton3 = document.getElementById("carrito3");
+
+boton3.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton4 = document.getElementById("carrito4");
+
+
+boton4.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton5 = document.getElementById("carrito5");
+
+
+boton5.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
+
+let  boton6 = document.getElementById("carrito6");
+
+boton6.onclick= function(){
+    contador++;
+    pElement.textContent=contador;
+}
